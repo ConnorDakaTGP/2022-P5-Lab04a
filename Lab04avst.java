d6881ecda725105789850bc482c01f9baff60411
Connor Donegan <connor.donegan@vtsd.com>
2022-10-03 12:14:52 -0400

    Lab 04

--- Lab04avst.java
+++ Lab04avst.java
@@ -2,13 +2,10 @@ public class Lab04avst
 {
     public static void main(String[] args)
         {
-            System.out.println("80p version");
-
-            System.out.println("Lab 04a Student Version\n");
+            System.out.println("Lab 04a Student 90 Point Version\n");
             double principal = 250000;
             double annualRate = 4.85;
             double numYears = 30;
-
             System.out.println("Principal: \t\t"+principal);
             System.out.println("Annual Rate: \t"+annualRate+"%");
             System.out.println("Number Of Years: "+numYears);
@@ -17,16 +14,18 @@ public class Lab04avst
             double monthlyRate = (annualRate / 100 / 12);
             double numMonths = numYears * 12;
 
-            //Top & bottom
             double numerator = Math.pow(1+monthlyRate,numMonths) * monthlyRate;
             double denominator = Math.pow(1+monthlyRate,numMonths) - 1;
 
             double monthPayment = ((numerator / denominator) * principal);
-
             System.out.println("Monthly Pay: $"+monthPayment);
 
-            double totalPay = monthPayment*numMonths;
+            //90 point
 
+            double totalPay = monthPayment*numMonths;
             System.out.println("Total Pay: $"+totalPay);
+
+            double interest = totalPay - principal;
+            System.out.println("Total Interest: $"+interest);
         }
 }
